144444501d5844f5dede8d114475598cfbdb5153
Vicki Crescencio <victoriaacw92@gmail.com>
2020-09-21 23:16:50 -0700

    Update and set up files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Container, Jumbotron } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Jumbotron>
+          <Container>
+              <h1>Search App</h1>
+              <p>This is a simple search app</p>
+          </Container>
+        </Jumbotron> 
+      </div>
+    );
+  }
+  }
+  
 
 export default App;
